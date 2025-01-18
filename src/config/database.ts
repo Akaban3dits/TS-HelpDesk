7285c11f954669ab03593e25ca3d406dee3897b0
Akaban3dits <newakbn12@gmail.com>
2025-01-17 23:40:35 -0500

    fix: Sequelize configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,19 +1,9 @@
 import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
 import * as path from "path";
 import { readdirSync } from "fs";
-import { Sequelize } from "sequelize-typescript";
 
 dotenv.config();
-
-const modelsPath = path.join(__dirname, "../models");
-const models: any[] = [];
-
-readdirSync(modelsPath)
-  .filter((file) => file.endsWith(".ts") && file !== "index.ts")
-  .forEach((file) => {
-    const model = require(path.join(modelsPath, file)).default;
-    models.push(model);
-  });
 
 const sequelize = new Sequelize({
   dialect: "postgres",
@@ -23,6 +13,23 @@ const sequelize = new Sequelize({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   logging: false,
+});
+
+const modelsPath = path.join(__dirname, "../models");
+
+readdirSync(modelsPath)
+  .filter((file) => file.endsWith(".ts") || file.endsWith(".js"))
+  .forEach((file) => {
+    const model = require(path.join(modelsPath, file)).default;
+    if (model && typeof model.init === "function") {
+      model.init(sequelize);
+    }
+  });
+
+Object.values(sequelize.models).forEach((model: any) => {
+  if (model.associate) {
+    model.associate(sequelize.models);
+  }
 });
 
 export default sequelize;
